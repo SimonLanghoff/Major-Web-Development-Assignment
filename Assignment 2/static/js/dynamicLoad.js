ca678245044589d8562edb7fef3aac27ccdfbacf
Simon Langhoff <simlanghoff@gmail.com>
2015-04-11 16:29:26 +0200

    Added first draft of part a, still need to convert homepage to 960 grid system and not bootstrap.

--- Assignment 2/static/js/dynamicLoad.js
+++ Assignment 2/static/js/dynamicLoad.js
@@ -2,3 +2,17 @@
 $(function(){
     $("#sidebar").load("sidebar.html");
 });
+
+// Load the header
+$(function(){
+    $("#header").load("header.html");
+});
+
+$(function(){
+    $("#sidebar_960").load("sidebar_960.html");
+});
+
+// Load the header
+$(function(){
+    $("#header_960").load("header_960.html");
+});
