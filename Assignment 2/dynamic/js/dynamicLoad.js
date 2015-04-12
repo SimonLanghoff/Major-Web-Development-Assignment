9eb9f0acb668b73ea63d942267373dbcf1317845
Simon Langhoff <simlanghoff@gmail.com>
2015-04-12 19:02:54 +0200

    Cleaned up the static part of the implementation. Also adding dynamic sketch.

---
+++ Assignment 2/dynamic/js/dynamicLoad.js
@@ -0,0 +1,19 @@
+// Load the sidebar html.
+$(function(){
+    $("#sidebar").load("sidebar.html");
+});
+
+// Load the header
+$(function(){
+    $("#header").load("header.html");
+});
+
+// Load the dynamic header
+$(function(){
+    $("#header_dynamic").load("header_dynamic.html");
+});
+
+// Load the header
+$(function(){
+    $("#footer").load("footer.html");
+});
