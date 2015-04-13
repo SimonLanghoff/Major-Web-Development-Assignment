ae2fea6e1b29269fe3475d29e0a7a2b6d4fd2300
Simon Langhoff <simlanghoff@gmail.com>
2015-04-13 13:59:40 +0200

    no message

---
+++ Assignment 2/static/js/functions.js
@@ -0,0 +1,9 @@
+$('.similar-movies-expanded').hover(function() {
+    $(this).toggleClass('hidden');
+})
+
+
+// For each image in image grid, add hover function (also save variable with class name
+// for each similar-movie-expanded, if hasclass hidden && hasClass (same movie) remove hidden.
+// else if hasClass hidden do nothing otherwise add class hidden
+//
